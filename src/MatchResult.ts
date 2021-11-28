276f332d4cedf556b67a1c74119a420c1a314d40
Scharif47 <61374955+Scharif47@users.noreply.github.com>
2021-11-28 19:22:22 +0100

    init commit

---
+++ src/MatchResult.ts
@@ -0,0 +1,6 @@
+// enumeration over collection of possible outcomes / close related values
+export enum MatchResult {
+  HomeWin = "H",
+  AwayWin = "A",
+  Draw = "D",
+}
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { MatchReader } from "./MatchReader";
+import { Summary } from "./Summary";
+
+const matchReader = MatchReader.fromCsv("football.csv");
+const summary = Summary.winsAnalysisHtmlReport("Man United");
+
+matchReader.load();
+summary.buildAndPrintReport(matchReader.matches);
